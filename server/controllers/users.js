c935acfb6cedc404d822e2e0767f84b87a155b85
fejiroofficial <fejiro.gospel@gmail.com>
2019-10-03 01:51:29 +0100

    Initial commit for a feature which allows a user login with the right credentials

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -20,9 +20,49 @@ static signup(req, res) {
         return res.status(201).json({
             success: true,
             message: 'Account created successfully',
-            token
+            data: {
+                token
+            }
         });
     });
+}
+
+  /**
+ * @function login
+ * @memberof UserController
+ * @static
+ */
+static login(req, res) {
+    const { body: { email, password } } = req;
+
+    User.findOne({ email })
+        .then(user => {
+            if (!user) {
+                return res.status(401).json({
+                    message: 'Incorrect Email or password'
+                });
+            }
+            user.comparePassword(password, user.password);
+            const token = jwt.sign({
+                id: user.id,
+                firstname: user.firstName,
+                lastname: user.lastName,
+                email: user.email,
+              }, process.env.SECRET_KEY, { expiresIn: '24hrs' });
+
+            return res.status(201).json({
+                success: true,
+                message: 'Login successful',
+                data: {
+                    token
+                }
+            });
+        }).catch(err => {
+            return res.status(500).json({
+                error: 'An error occured!',
+                success: false
+            });
+        });
 }
 
 }
